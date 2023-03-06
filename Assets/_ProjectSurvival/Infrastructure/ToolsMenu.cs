663839979752082487473fa5c1ef8a8fada7129d
Tim Markin <timopro16@gmail.com>
2023-03-06 19:05:22 +0300

    after build fixes (camera), ui positioning

--- Assets/_ProjectSurvival/Infrastructure/ToolsMenu.cs
+++ Assets/_ProjectSurvival/Infrastructure/ToolsMenu.cs
@@ -5,7 +5,7 @@ namespace _ProjectSurvival.Infrastructure
 {
     public class ToolsMenu : MonoBehaviour
     {
-        [MenuItem("Survival Tools/Drop State")]
+        //[MenuItem("Survival Tools/Drop State")]
         static void Init()
         {
             GameProgressHandler.DeleteFile();
