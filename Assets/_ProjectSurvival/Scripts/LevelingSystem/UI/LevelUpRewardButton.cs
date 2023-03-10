4fac24dc61c4a5ab3554ac69b0a7a7698f54a7f8
СОН КОТА <kota_workshop@mail.ru>
2023-03-10 11:49:30 +0300

    Leveling refactoring (namespaces)

--- Assets/_ProjectSurvival/Scripts/LevelingSystem/UI/LevelUpRewardButton.cs
+++ Assets/_ProjectSurvival/Scripts/LevelingSystem/UI/LevelUpRewardButton.cs
@@ -1,52 +1,53 @@
-using System.Collections.Generic;
 using _ProjectSurvival.Scripts.LevelingSystem.Rewards;
-using _ProjectSurvival.Scripts.LevelingSystem.UI;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using Zenject;
 
-public class LevelUpRewardButton : MonoBehaviour
+namespace _ProjectSurvival.Scripts.LevelingSystem.UI
 {
-    [SerializeField] private Button _button;
-    [SerializeField] private Text _attackLabel;
-    private LevelUpWindow _levelUpWindow;
-    private List <IRewardGiver> _rewardGiver = new List<IRewardGiver>();
-    private IReward _reward;
-    private RewardType _rewardType;
-
-    private void OnDestroy()
+    public class LevelUpRewardButton : MonoBehaviour
     {
-        _button.onClick.RemoveListener(OnCick);
-    }
+        [SerializeField] private Button _button;
+        [SerializeField] private Text _attackLabel;
+        private LevelUpWindow _levelUpWindow;
+        private List<IRewardGiver> _rewardGiver = new List<IRewardGiver>();
+        private IReward _reward;
+        private RewardType _rewardType;
 
-    public void Init(LevelUpWindow levelUpWindow, List <IRewardGiver> rewardGiver = null)
-    {
-        _levelUpWindow = levelUpWindow;
-        _rewardGiver = rewardGiver;
-        _button.onClick.AddListener(OnCick);
-    }
+        private void OnDestroy()
+        {
+            _button.onClick.RemoveListener(OnCick);
+        }
 
-    public void Hide()
-    {
-        gameObject.SetActive(false);
-    }
+        public void Init(LevelUpWindow levelUpWindow, List<IRewardGiver> rewardGiver = null)
+        {
+            _levelUpWindow = levelUpWindow;
+            _rewardGiver = rewardGiver;
+            _button.onClick.AddListener(OnCick);
+        }
 
-    public void ShowReward(IReward reward)
-    {
-        _reward = reward;
-        _rewardType = _reward.GetRewardType();
-        _attackLabel.text = _reward.Title;
-        gameObject.SetActive(true);
-    }
+        public void Hide()
+        {
+            gameObject.SetActive(false);
+        }
 
-    private void OnCick()
-    {
-        if (_reward != null && _rewardGiver != null)
-            foreach (var rewardGiver in _rewardGiver)
-            {
-                rewardGiver.GiveReward(_reward);
-            }
-           // _rewardGiver.GiveReward(_reward);
-        _levelUpWindow.CloseLevelUpWindow();
+        public void ShowReward(IReward reward)
+        {
+            _reward = reward;
+            _rewardType = _reward.GetRewardType();
+            _attackLabel.text = _reward.Title;
+            gameObject.SetActive(true);
+        }
+
+        private void OnCick()
+        {
+            if (_reward != null && _rewardGiver != null)
+                foreach (var rewardGiver in _rewardGiver)
+                {
+                    rewardGiver.GiveReward(_reward);
+                }
+            // _rewardGiver.GiveReward(_reward);
+            _levelUpWindow.CloseLevelUpWindow();
+        }
     }
 }
