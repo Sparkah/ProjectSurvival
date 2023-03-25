7d5ade5acf8800c1b94a2550ff70b669a9e82caa
СОН КОТА <kota_workshop@mail.ru>
2023-03-25 14:53:30 +0300

    Enemy levels pictures and lock state to session end added

--- Assets/_ProjectSurvival/Scripts/GameFlow/Statistics/UI/EnemyLevelResultUI.cs
+++ Assets/_ProjectSurvival/Scripts/GameFlow/Statistics/UI/EnemyLevelResultUI.cs
@@ -5,14 +5,21 @@ namespace _ProjectSurvival.Scripts.GameFlow.Statistics
 {
     public class EnemyLevelResultUI : MonoBehaviour
     {
+        [Header("Text UI")]
         [SerializeField] private Text _defeatedCountText;
+        [SerializeField] private string _priceLockedText = "???";
         [SerializeField] private Text _priceText;
         [SerializeField] private Text _totalCurrencyText;
+        [Header("Picture")]
+        [SerializeField] private Image _enemiesImage;
+        [SerializeField] private Sprite _lockedPicture;
+        [SerializeField] private Sprite _unlockedPicture;
 
         public void FillData(int defeatedCount, float price, float totalCurrency)
         {
             _defeatedCountText.text = defeatedCount.ToString();
-            _priceText.text = price.ToString();
+            _enemiesImage.sprite = defeatedCount > 0 ? _unlockedPicture : _lockedPicture;
+            _priceText.text = defeatedCount > 0 ? price.ToString() : _priceLockedText;
             _totalCurrencyText.text = Mathf.FloorToInt(totalCurrency).ToString();
         }
     }
