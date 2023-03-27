4c491c93474e67124a6088b8b23ff5f3a3b4f292
СОН КОТА <kota_workshop@mail.ru>
2023-03-27 16:19:07 +0300

    DoTween warning fix for tutorial highlight

--- Assets/_ProjectSurvival/Scripts/Tutorial/TweenableButton.cs
+++ Assets/_ProjectSurvival/Scripts/Tutorial/TweenableButton.cs
@@ -18,6 +18,8 @@ namespace _ProjectSurvival.Scripts.Tutorial
         private void OnDestroy()
         {
             StopTween();
+            if (_button)
+                _button.transform.DOKill();
         }
 
         public void Tween()
